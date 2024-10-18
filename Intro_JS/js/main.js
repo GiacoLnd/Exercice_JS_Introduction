90f41c62ede12a7f835bb5645737c204fd20fd8f
Giacomo Landolfo <giacomo.6719@gmail.com>
2024-10-18 16:03:13 +0200

    Exercice Intro JS final ver.

--- Intro_JS/js/main.js
+++ Intro_JS/js/main.js
@@ -6,21 +6,19 @@ const taskContainer = document.querySelector("#todoCards");
 
 const counter = document.getElementById('count')
 
-let count = 1
 
 function addTask(){
     const newTask = taskCard.cloneNode(true)
     const newDelBtn = newTask.querySelector('.delBtn')
     const newTextArea = newTask.querySelector('.task')
 
-    count++;
-    counter.textContent = `Tâche(s) en attente : ${count}`
     
     newTextArea.value = "New Task"
     newDelBtn.addEventListener('click', function() {
         deleteTask(newTask);
     })
     taskContainer.appendChild(newTask)
+    updateCounter()
 }
 
 
@@ -32,9 +30,12 @@ delBtn.addEventListener('click', function(){
 
 function deleteTask(task){
     task.remove();
-    count--;
-    counter.textContent = `Tâche(s) en attente : ${count}`
+    updateCounter()
 }
 
 
 
+function updateCounter(){
+    const nbZone = taskContainer.children.length;
+    counter.textContent = "Tâche(s) en attente : " + nbZone
+}
